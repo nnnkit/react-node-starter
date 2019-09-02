0422039339c1ffa5f70b7d55a73037959078f542
nnnkit <iankit17@gmail.com>
2019-09-02 18:41:48 +0530

    add basic setup for ejs

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const app = express()
 const config = require('./config')
 
 app.use(express.static('public'))
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-  res.send({ main: 'Hello' })
+  res.render('index', { message: 21 })
 })
 app.listen(config.port, () => {
   console.log(`Running at http://localhost:${config.port}`)
